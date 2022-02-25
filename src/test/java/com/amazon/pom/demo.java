b2cb7f5c8c59c3764f1ba5bf9d1d65e8370d4e3e
TGAmrithaP <amritha.p@truglobal.com>
2022-02-25 15:05:18 +0530

    demo

---
+++ src/test/java/com/amazon/pom/demo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.amazon.pom;
+
+/**
+ * @author TG1690
+ *
+ */
+public class demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("jhhfffffffffffffffffffffffffffffff");
+	}
+
+}
